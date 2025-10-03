67ec68f9fdd41b317bf6063fd5df1d58788279be
Sumit-Kadam-code <sumitkadam825@gmail.com>
2025-10-03 22:33:27 +0530

    Add files via upload

---
+++ Assignment0/Q2area.c.c
@@ -0,0 +1,11 @@
+//---Problem 2:  Find Area Of Circle ---
+
+#include<stdio.h>
+void main() {
+	printf("\n Problem 2: Area Of Circle \n");
+	
+	float radius = 3.5;
+ float	area = 3.14 * radius * radius ;
+ printf(" The area of circle with radius %f is %f\n",radius,area);
+	
+}
